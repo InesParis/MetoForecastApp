d36caf3233bef7f2eb568d733cee5a795895b194
InesParis <inesparisarranz@gmail.com>
2024-07-18 18:28:51 +0200

    forecast inserting js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ function refreshWeather(response) {
   currentWeatherIconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-app-temperature-icon"/>`;
 
   forecastCityWeek(response.data.city);
+  displayForecastWeek(response.data.city);
 }
 function forecastWeek(response) {
   let maxTemperatureElement = document.querySelector("#max-temperature");
@@ -71,6 +72,43 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Madrid");
+
+function displayForecastWeek(city) {
+  let apiKeyForecast = "4f2360cc5d2fbf9f02a9o90ddad3f50t";
+  let apiUrlWeek = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKeyForecast}`;
+
+  axios.get(apiUrlWeek).then(displayForecast);
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+       <div class="weather-forecast-date">${formatDay(day.time)}</div>
+       <img src="${day.condition.icon_url}"class="weather-forecast-icon"/>
+       <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature-high"><strong>${Math.round(
+          day.temperature.maximum
+        )}</strong></div>
+         <div class="weather-forecast-temperature-low">${Math.round(
+           day.temperature.minimum
+         )}</div>
+       </div>`;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 function forecastCityWeek(city) {
   let apiKeyForecast = "4f2360cc5d2fbf9f02a9o90ddad3f50t";
